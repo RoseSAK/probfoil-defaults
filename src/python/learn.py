315f0ae1ad2ea545cebde9f9176f9644f744d559
Anton Dries <anton.dries@gmail.com>
2013-07-01 19:37:59 +0200

    Improved cached version

--- src/python/learn.py
+++ src/python/learn.py
@@ -1,35 +1,74 @@
 #! /usr/bin/env python3
 
-
-def learn(l) :
-        
-    H = l.RuleSet(l.target)
-    newH = None
+def localStop(H) :
     
-    h = l.target
-    stop = False
-    while (newH == None or not stop) :
-        stop = True
-        r = l.Rule(h,[])
-        while not l.localStop(H,r) :
-            lit = argmax(r.refine(l.data),lambda lt : l.localScore(H, r+lt)) 
+    TP_p = H.TP1
+    TP_c = H.TP
+    
+    FP_p = H.FP1
+    FP_c = H.FP
+    
+    #print H.POS, H.NEG
+    
+    return ( TP_c - TP_p == 0 ) or ( FP_c - FP_p == 0 )
+
+def localScore(rs) :
+    m = 1
+    
+    m_estimate_c = (rs.TP + m * (rs.P / (rs.N + rs.P))) / (rs.TP + rs.FP + m) 
+    
+    m_estimate_p = (rs.TP1 + m * (rs.P / (rs.N + rs.P))) / (rs.TP1 + rs.FP1 + m) 
+    
+    return m_estimate_c - m_estimate_p
+    
+def globalScore(rs) :
+    return (rs.TP + rs.TN ) / (rs.TP + rs.TN + rs.FP + rs.FN)
+    
+def learn(H) :
+
+    score_H = globalScore(H)
+
+    while True :
+        H.pushRule()
+        while not localStop(H) :
+            lit = best_literal( H, H.rules[-1].refine(H.data), lambda H : localScore(H) ) 
             #if lit == None : return H
-            r += lit
-        n = len(r.body)
+            H.pushLiteral(lit)
+
         
-        i = argmax( range(1,n+1), lambda i : l.localScore(H, l.Rule(l.target,r.body[:i]) ))
+        # FIXME pruning step
+        # n = len(r.body)
+        # 
+        # i = argmax( range(1,n+1), lambda i : localScore(H, H.newRule(target,r.body[:i]), data ))
+        # 
+        # c = H.newRule( r.head, r.body[:i] )        
+        # newH = H + c
         
-        c = l.Rule( r.head, r.body[:i] )
+        score_nH = globalScore(H)
         
-        newH = H + c
-        
-        score_H = l.globalScore(H)
-        score_nH = l.globalScore(newH)
-        
-        if (l.globalScore(H) < l.globalScore(newH)) :
-            H = newH
-            stop = False
+        #print score_H, score_nH
+        if (score_H >= score_nH) :
+            H.popRule()
+            break
+        else :
+            score_H = score_nH
     return H
+
+def best_literal( H, generator, score_func ) :
+    max_score = None
+    max_x = None
+    
+    for lit in generator :
+        H.pushLiteral(lit)
+        current_score = score_func(H)
+        if max_score == None or max_score < current_score :
+            max_score, max_x = current_score, lit
+        H.popLiteral()
+#    lit = argmax(r.refine(data), lambda lt : localScore(H, r+lt, data)) 
+    return max_x
+
+
+
         
 def argmax( generator, score ) :
     max_score = None
@@ -47,45 +86,4 @@ def argmax( generator, score ) :
 #    print '==>', max_x, max_score
     return max_x
         
-import simple
-        
-class SimpleLearn(object) :
-    
-    def __init__(self, data, target) :
-        self.RuleSet = simple.RuleSet
-        self.Rule = simple.Rule
-        self.data = data
-        self.target = target
-        
-        rules = self.RuleSet(target)
-        TP, self.N, FN, self.P = map(len,rules.evaluate(data))
 
-    def localStop(self, rs, rule) :
-        rs2 = rs + rule
-        
-        rsTP, rsTN, rsFP, rsFN = map(len,rs.evaluate(self.data))
-        rs2TP, rs2TN, rs2FP, rs2FN = map(len,rs2.evaluate(self.data))
-        
-        # print rs
-        # print '---'
-        # print rs2
-        # 
-        # print rsTP, rsTN, rsFP, rsFN,         rs2TP, rs2TN, rs2FP, rs2FN
-        
-        return ( rs2TP - rsTP == 0 ) or ( rs2FP - rsFP == 0 )
-    
-    def localScore(self, ruleset, rule) :
-        rs2score = self.m_estimate(ruleset+rule)
-        rsscore = self.m_estimate(ruleset)
-        #print rs2score, rsscore, rule
-        return rs2score - rsscore
-        
-    def globalScore(self, rs) :
-        rsTP, rsTN, rsFP, rsFN = map(len,rs.evaluate(self.data))
-        return float(rsTP + rsTN ) / (rsTP + rsTN + rsFP + rsFN)
-        
-    def m_estimate(self, rs, m=1) :
-        rsTP, rsTN, rsFP, rsFN = map(len,rs.evaluate(self.data))
-        #print rsTP, rsTN, rsFP, rsFN
-        return float(rsTP + m * (self.P / (self.N + self.P))) / (rsTP + rsFP + m)
-        
--- src/python/simple.py
+++ src/python/simple.py
@@ -6,19 +6,131 @@ Example = list
 
 Literal = namedtuple('Literal',  ['pos', 'val'] )
 
-class KnowledgeBase(list) :
+class KnowledgeBase(object) :
     
     def __init__(self, header, data) :
-        super(KnowledgeBase,self).__init__(data)
+        super(KnowledgeBase,self).__init__()
+        
+        self.data = data
         
         self.fields = dict( [ (x,i) for i,x in enumerate(header) ] )
+        self.header = header
         self.values = [ set([ row[col] for row in data ]) for col in range(0,len(data[0])) ]
+    
+    def __getitem__(self, index) :
+        return self.data[index]
+        
+    def __iter__(self) :
+        return iter(range(0,len(self.data)))
     
     def field(self, name) :
         return self.fields[name]
     
     def field_values(self, name) :
         return self.values[self.field(name)]
+
+
+class RuleSet(object) :
+    
+    def __init__(self, target, data) :
+        self.target = target
+        self.rules = []
+        self.data = data
+        
+        evaluated = ( [], [] )
+        for ex in self.data :
+            h, b = Rule(target).evaluate(self.data[ex])
+            evaluated[h].append( ex )
+        negatives, positives = evaluated
+        
+        self.P = float(len(positives))
+        self.N = float(len(negatives))
+        
+        self.POS = [positives]  # should be set of positive examples
+        self.NEG = [negatives]  # should be set of negative examples
+                
+    def getTP(self) :
+        return float(self.P - self.FN)
+    
+    def getTN(self) :
+        return float(len(self.NEG[0]))
+    
+    def getFP(self) :
+        return float(self.N - self.TN)
+    
+    def getFN(self) :
+        return float(len(self.POS[0]))
+
+    def getTP1(self) :
+        return float(self.P - self.FN1)
+    
+    def getTN1(self) :
+        return float(len(self.NEG[0]) + len(self.NEG[-1]))
+    
+    def getFP1(self) :
+        return float(self.N - self.TN1)
+    
+    def getFN1(self) :
+        return float(len(self.POS[0]) + len(self.POS[-1]))
+
+        
+    TP = property(getTP)    
+    TN = property(getTN)
+    FP = property(getFP)
+    FN = property(getFN)
+
+    TP1 = property(getTP1)    
+    TN1 = property(getTN1)
+    FP1 = property(getFP1)
+    FN1 = property(getFN1)
+
+        
+    def pushRule(self, rule=None) :
+        if rule == None : rule = self.newRule()
+        
+        evaluated = [[],[]]
+        for example in self.POS[0] :
+            h, b = rule.evaluate(self.data[example])
+            evaluated[b].append(example)
+        self.POS[0] = evaluated[0]
+        self.POS.append( evaluated[1] )
+        
+        evaluated = [[],[]]
+        for example in self.NEG[0] :
+            h, b = rule.evaluate(self.data[example])
+            evaluated[b].append(example)
+        self.NEG[0] = evaluated[0]
+        self.NEG.append( evaluated[1] )                    
+        
+        self.rules.append(rule)
+
+    def popRule(self) :
+        self.POS[0] = self.POS[0]+self.POS[-1]
+        self.POS.pop(-1)
+        
+        self.NEG[0] = self.NEG[0]+self.NEG[-1]
+        self.NEG.pop(-1)
+        
+        self.rules.pop(-1)
+        
+    def pushLiteral(self, literal) :
+        # quick implementation
+        rule = self.rules[-1]
+        self.popRule()
+        self.pushRule( rule + literal )
+    
+    def popLiteral(self) :
+        # quick implementation
+        rule = self.rules[-1]
+        self.popRule()
+        rule.body = rule.body[:-1]
+        self.pushRule( rule )
+        
+    def newRule(self) :
+        return Rule(self.target)
+        
+    def __str__(self) :
+        return '\n'.join(map(str,self.rules))
         
 class Rule(object) :
     
@@ -56,55 +168,56 @@ class Rule(object) :
     def __add__(self, lit) :
         return Rule(self.head, self.body + [lit])
     
-class RuleSet(object) :
-    
-    def __init__(self, target) :
-        self.rules = []
-        self.target = target
-        
-    def evaluate_one(self, example) :
-        for rule in self.rules :
-            h, b = rule.evaluate(example)
-            if b : return h, True
-            
-        h, b = Rule(self.target).evaluate(example)
-    
-        return h, False
-        
-    def evaluate(self, data) :
-        TP = []
-        TN = []
-        FP = []
-        FN = []
-    
-        for i, ex in enumerate(data) :
-            h, b = self.evaluate_one(ex)
-            
-            if h :  # positive 
-                if b :
-                    TP += [i]
-                else :
-                    FN += [i]
-            else :
-                if b :
-                    FP += [i]
-                else : 
-                    TN += [i]
-        return TP, TN, FP, FN        
-        
-    def __iadd__(self, rule) :
-        self.rules.append(rule)
-        return self
-        
-    def __add__(self, rule) :
-        if type(rule) == list :
-            raise RuntimeError
-        result = RuleSet(self.target)
-        result.rules = self.rules + [rule]
-        return result
-        
-    def __str__(self) :
-        return '\n'.join(map(str,self.rules))
+# class RuleSet(object) :
+#     
+#     def __init__(self, target) :
+#         self.rules = []
+#         self.target = target
+#         self.newRule = Rule
+#         
+#     def evaluate_one(self, example) :
+#         for rule in self.rules :
+#             h, b = rule.evaluate(example)
+#             if b : return h, True
+#             
+#         h, b = Rule(self.target).evaluate(example)
+#     
+#         return h, False
+#         
+#     def evaluate(self, data) :
+#         TP = []
+#         TN = []
+#         FP = []
+#         FN = []
+#     
+#         for i, ex in enumerate(data) :
+#             h, b = self.evaluate_one(ex)
+#             
+#             if h :  # positive 
+#                 if b :
+#                     TP += [i]
+#                 else :
+#                     FN += [i]
+#             else :
+#                 if b :
+#                     FP += [i]
+#                 else : 
+#                     TN += [i]
+#         return TP, TN, FP, FN        
+#         
+#     def __iadd__(self, rule) :
+#         self.rules.append(rule)
+#         return self
+#         
+#     def __add__(self, rule) :
+#         if type(rule) == list :
+#             raise RuntimeError
+#         result = RuleSet(self.target)
+#         result.rules = self.rules + [rule]
+#         return result
+#         
+#     def __str__(self) :
+#         return '\n'.join(map(str,self.rules))
     
 def read_file(filename) :
     line_parse = lambda line : line.strip().split(',')
@@ -118,29 +231,21 @@ def test(args=[]) :
     data = read_file('../../data/test.dat')
     kb = KnowledgeBase(data[0], data[1:])
 
-    from learn import learn, SimpleLearn
-    # r0 = learn(SimpleLearn(kb,(kb.field('C'),'0')))
-    # print 'RESULT:'
-    # print r0
-    # 
-    # r1 = learn(SimpleLearn(kb,(kb.field('C'),'1')))
-    # 
-    # print 'RESULT:'
-    # print r1
-
+    
 
     data = read_file('../../data/zoo.dat')
     kb = KnowledgeBase(data[0], data[1:])
 
-    from learn import learn, SimpleLearn
+    from learn import learn
 
-    print learn(SimpleLearn(kb,(kb.field('type'),'1')))    
-    print learn(SimpleLearn(kb,(kb.field('type'),'2')))    
-    print learn(SimpleLearn(kb,(kb.field('type'),'3')))    
-    print learn(SimpleLearn(kb,(kb.field('type'),'4')))    
-    print learn(SimpleLearn(kb,(kb.field('type'),'5')))    
-    print learn(SimpleLearn(kb,(kb.field('type'),'6')))    
-    print learn(SimpleLearn(kb,(kb.field('type'),'7')))        
+    print learn(RuleSet((kb.field('type'),'1'), kb))    
+    print learn(RuleSet((kb.field('type'),'2'), kb))
+    print learn(RuleSet((kb.field('type'),'3'), kb))   
+    print learn(RuleSet((kb.field('type'),'4'), kb))   
+    print learn(RuleSet((kb.field('type'),'5'), kb))   
+    print learn(RuleSet((kb.field('type'),'6'), kb))   
+    print learn(RuleSet((kb.field('type'),'7'), kb))   
+    
     
 if __name__ == '__main__' :
     import sys    
