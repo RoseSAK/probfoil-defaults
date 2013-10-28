8ad072eefabc662a161c3f78c0042af07ff56c50
Anton Dries <anton.dries@gmail.com>
2013-10-28 16:29:34 +0100

    Fix in arff_to_pl converter (no probabilities in scientific notation, learn spec line)

--- data/arff_to_pl.py
+++ data/arff_to_pl.py
@@ -12,10 +12,16 @@ def main(filenames) :
                 line_num = 0
                 for line_in in file_in :
                     line_in = line_in.strip()
-                    if line_in and not line_in.startswith('@') :
+                    if line_in and not line_in.startswith('@') and not line_in.startswith('#') :
                         values = list(map(float,line_in.split(',')))
                         num_atts = len(values)
-                        line_out = '\n'.join( '%s::att%s(%s).' % (val, att, line_num) for att, val in enumerate(values) ) + '\n\n'
+                        line_out = '\n'.join( '%.6f::att%s(%s).' % (float(val), att, line_num) for att, val in enumerate(values) ) + '\n\n'
+                        
+                        if line_num == 0 :
+                            # write LEARN header
+                            line_out = '%%LEARN att%s/1 ' % (len(values)-1) + ' '.join( 'att%s/+' % att for att, val in enumerate(values[:-1]) )  + '\n'
+                            file_out.write(line_out)
+                        line_out = '\n'.join( '%.6f::att%s(%s).' % (float(val), att, line_num) for att, val in enumerate(values) ) + '\n\n'
                         file_out.write(line_out)
                         line_num += 1
                 line_out = '\n'.join( 'base(att%s(id)).' % att for att in range(0, num_atts) ) + '\n\n'
