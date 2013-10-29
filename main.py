45a6cac1b8f7b8740a0598ea8c377d820f1eb588
Anton Dries <anton.dries@gmail.com>
2013-10-29 10:00:47 +0100

    Added support for ARFF files (disabled specialized propositional learner).

--- main.py
+++ main.py
@@ -84,17 +84,20 @@ def main(arguments) :
     modes = list(map(lambda x : Literal(*x.split('/')), args.modes))
       
     learn_time = time.time()  
+
     
     
     with open(args.log, 'w') as Log.LOG_FILE :
      with WorkEnv(PROBLOGPATH=PROBLOGPATH, verbose=args.verbose) as env :    # Set up a temporary working directory
+
+      if args.input.endswith('.arff') :
+          pl_file = env.tmp_path(os.path.splitext(os.path.split( args.input )[1])[0] + '.pl')
+          arff_to_pl( args.input, pl_file )
+          args.input = pl_file
+
       with Log('log', **parameters) :
        with Timer(category='') :    
-        if args.input.endswith('.arff') :
-            print ('USING PROPOSITIONAL ALGORITHM')
-            p = PropPrologInterface(env)
-        else :
-            p = YapPrologInterface(env)
+        p = YapPrologInterface(env)
         
         p.loadData(args.input)
         
@@ -148,6 +151,28 @@ def main(arguments) :
        print(Timer.showTimers())        
        print('total',' => ', learn_time, 's', sep='')
        with Log('timers', total=learn_time, **Timer.TIMERS) : pass
+
+def arff_to_pl(filename_in, filename_out) :
+    with open(filename_in) as file_in :
+        with open(filename_out, 'w') as file_out :
+            line_num = 0
+            for line_in in file_in :
+                line_in = line_in.strip()
+                if line_in and not line_in.startswith('@') and not line_in.startswith('#') :
+                    values = list(map(float,line_in.split(',')))
+                    num_atts = len(values)
+                    line_out = '\n'.join( '%.6f::att%s(%s).' % (float(val), att, line_num) for att, val in enumerate(values) ) + '\n\n'
+                    
+                    if line_num == 0 :
+                        # write LEARN header
+                        line_out = '%%LEARN att%s/1 ' % (len(values)-1) + ' '.join( 'att%s/+' % att for att, val in enumerate(values[:-1]) )  + '\n'
+                        file_out.write(line_out)
+                    line_out = '\n'.join( '%.6f::att%s(%s).' % (float(val), att, line_num) for att, val in enumerate(values) ) + '\n\n'
+                    file_out.write(line_out)
+                    line_num += 1
+            line_out = '\n'.join( 'base(att%s(id)).' % att for att in range(0, num_atts) ) + '\n\n'
+            file_out.write(line_out)    
+
 
 if __name__ == '__main__' :
     main(sys.argv[1:])    
